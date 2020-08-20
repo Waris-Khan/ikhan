f5e4dbae973832fb816cbe5ecb0d9381a85f7765
Waris-Khan <k.waris14@gmail.com>
2020-08-20 19:41:52 +0500

    Updates

---
+++ precache-manifest.b2067208fe1293bb8b5c16bf74c63e89.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de6a31f1ef278175b31a4cedd9aba63",
+    "url": "/ikhan/index.html"
+  },
+  {
+    "revision": "b14880bc9d9591652ad6",
+    "url": "/ikhan/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "e3e6d1e8ee9455445bc8",
+    "url": "/ikhan/static/css/main.5c38af8d.chunk.css"
+  },
+  {
+    "revision": "b14880bc9d9591652ad6",
+    "url": "/ikhan/static/js/2.e748517f.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/ikhan/static/js/2.e748517f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e6d1e8ee9455445bc8",
+    "url": "/ikhan/static/js/main.873a3502.chunk.js"
+  },
+  {
+    "revision": "616e64689fa56a1f85f9",
+    "url": "/ikhan/static/js/runtime-main.e63c26b6.js"
+  },
+  {
+    "revision": "400a33e1df3014958e5d88640b283146",
+    "url": "/ikhan/static/media/center.400a33e1.jfif"
+  },
+  {
+    "revision": "62ed017bccd6de6f01057631919baa65",
+    "url": "/ikhan/static/media/cv_pdf.62ed017b.pdf"
+  },
+  {
+    "revision": "25dc0d89b50bcad92137151f0b550498",
+    "url": "/ikhan/static/media/khaleej-ships.25dc0d89.png"
+  },
+  {
+    "revision": "d52d13b4548fb6848821aff9c13aa7a6",
+    "url": "/ikhan/static/media/starter-site.d52d13b4.png"
+  },
+  {
+    "revision": "6876d474fc93c798eaebb4710357f117",
+    "url": "/ikhan/static/media/starter-site2.6876d474.png"
+  },
+  {
+    "revision": "5a731de3ff5997dbb3b5100189ce39a0",
+    "url": "/ikhan/static/media/tech-automation.5a731de3.png"
+  },
+  {
+    "revision": "e23315419966a52862cd14c1a4e11b44",
+    "url": "/ikhan/static/media/todo-list.e2331541.png"
+  },
+  {
+    "revision": "5b6f75918e7c127b7de8ee8eefdad280",
+    "url": "/ikhan/static/media/white-logo.5b6f7591.png"
+  }
+]);
